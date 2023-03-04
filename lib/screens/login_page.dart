8f148e7f6ddf8da1d5399727bb85bd9393e56ec7
nandam2003 <rishith820@gmail.com>
2023-03-05 00:57:35 +0530

    general update

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/services/auth_services.dart';
 
--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -1,8 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_application_1/screens/home.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import '../login_page.dart';
 
 class AuthService {
   signInWithGoogle() async {
